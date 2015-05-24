124e63b1fc2f6d6343acb356395ecf4c38ed0faf
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-05-24 14:36:01 +0300

    Lexer? (parser vs lexer?) parses nested atoms

--- src/whispy_lispy/lexer.py
+++ src/whispy_lispy/lexer.py
@@ -6,10 +6,47 @@ import re
 if six.PY2:
     str = unicode
 
+def get_atoms(text):
+    """Return the "atom" as a possibly nested list
+
+    An atom means the symbols surrounded by (), possibly containing other atoms
+
+    :param text: text surrounded by with '(' at the start and ')' at the end
+    :return:
+    """
+    # find all highest level atoms
+    #  (eg. atom1="asdf" and atom2="dede (1 2)" from "(asdf)(dede (1 2))"
+    # feed them to get_tokens
+    remaining_text = text
+
+    atoms = []
+    while remaining_text:
+        open_count = 0
+        for idx, char in enumerate(remaining_text):
+            if char == '(':
+                open_count += 1
+            elif char == ')':
+                open_count -= 1
+                if open_count == 0:
+                    found_atom = remaining_text[1:idx].lstrip()
+                    new_tokens = get_tokens(found_atom)
+                    if new_tokens:
+                        atoms.append(new_tokens)
+                    remaining_text = remaining_text[idx + 1:].lstrip()
+                    break
+            elif open_count == 0:
+                new_tokens = get_tokens(remaining_text)
+                atoms.extend(new_tokens)
+                remaining_text = ''
+
+    return atoms
+
+
 TOKEN_TYPES = (
     (float, re.compile('[0-9]+\.[0-9]+')),
     (int, re.compile('[0-9]+')),
-    (str, re.compile('[a-zA-Z_]+'))
+    (str, re.compile('[a-zA-Z_]+')),
+    (get_atoms, re.compile('\(.*\)'))
 )
 
 def get_tokens(text):
@@ -19,10 +56,17 @@ def get_tokens(text):
     :rtype: list[str]
     """
     tokens = []
-    for converter, pattern in TOKEN_TYPES:
-        result = pattern.match(text)
-        if result:
-            tokens.append(converter(result.string))
-            break
+    remaining_text = text
+
+    while remaining_text:
+        for converter, pattern in TOKEN_TYPES:
+            result = pattern.match(remaining_text)
+            if result:
+                start, end = result.span()
+                new_tokens = converter(remaining_text[start:end])
+                if not isinstance(new_tokens, list):
+                    new_tokens = [new_tokens]
+                tokens.extend(new_tokens)
+                remaining_text = remaining_text[end:].lstrip()
 
     return tokens
--- tests/test_whispy_lispy.py
+++ tests/test_whispy_lispy.py
@@ -16,3 +16,25 @@ class LexerTestCase(unittest.TestCase):
 
     def test_lexer_parses_symbol(self):
         self.assertEqual(lexer.get_tokens('as_df'), ['as_df'])
+
+    def test_lexer_parses_all_known_symbol_types(self):
+        self.assertEqual(
+            lexer.get_tokens('dada 3.4 44 rere 6'),
+            ['dada', 3.4, 44, 'rere', 6])
+
+    def test_lexer_parses_simple_atom(self):
+        self.assertEqual(lexer.get_tokens('(asdf 3)'), [['asdf', 3]])
+
+    def test_lexer_omits_newline(self):
+        self.assertEqual(lexer.get_tokens('asdf\nzxcv'), ['asdf', 'zxcv'])
+
+    def test_lexer_parses_multiple_non_nested_atoms(self):
+        self.assertEqual(
+            lexer.get_tokens('(asdf 3) (zxcv 4)'),
+            [['asdf', 3], ['zxcv', 4]])
+
+    def test_lexer_parses_multiple_nested_atoms(self):
+        self.assertEqual(
+            lexer.get_tokens('(a 1 (2 3 (4 b)) c (2 2 (1 a))) (a 2)'),
+            [['a', 1, [2, 3, [4, 'b']], 'c', [2, 2, [1, 'a']]], ['a', 2]]
+        )
