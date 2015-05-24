2dd1776039a0e697842d5344ae63236010bb36b2
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-05-24 21:20:24 +0300

    little import refactoring

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals, absolute_import
 
 import unittest
 
-from whispy_lispy import interpreter, parser
-import whispy_lispy.ast
+from whispy_lispy import interpreter, parser, ast
+
 
 
 class InterpreterTest(unittest.TestCase):
@@ -17,13 +17,13 @@ class InterpreterTest(unittest.TestCase):
     def test_interpret_return_last_value(self):
         self.assertEqual(interpreter.interpret([3, 4, 5]), 5)
 
-    def test_assigment_changes_global_scope(self):
+    def test_assignment_changes_global_scope(self):
         scope = {}
-        interpreter.interpret([whispy_lispy.ast.Assign('x', 3)], scope)
+        interpreter.interpret([ast.Assign('x', 3)], scope)
         self.assertEqual(scope['x'], 3)
 
     def test_assignments_change_scope_and_interpreter_returns_value(self):
         scope = {}
-        result = interpreter.interpret([whispy_lispy.ast.Assign('x', 6), 4], scope)
+        result = interpreter.interpret([ast.Assign('x', 6), 4], scope)  # noqa
         self.assertEqual(scope['x'], 6)
         self.assertEqual(result, 4)
