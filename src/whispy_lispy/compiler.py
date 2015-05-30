06919099459e386525b4909dbcc23f56d4e2adf7
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-05-30 17:23:38 +0300

    compiler intention

---
+++ src/whispy_lispy/compiler.py
@@ -0,0 +1,5 @@
+# -*- coding utf-8 -*-
+"""This module marks the intention to someday convert the AST emitted by the
+parser into some intermediate representation (or assembler core?) to maybe
+run on some virtual machines, like the python interpreter, JVM, etc.
+"""
