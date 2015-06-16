acbe04843716cbbdaae3b86f0172b84a10f16cdd
vladiibine <vlad.ardelean@3pillarglobal.com>
2015-06-16 20:13:48 +0300

    comments on the new syntax - still buggy and will most likely not get a lot more support

--- tests/test_lexer/test_optionals.py
+++ tests/test_lexer/test_optionals.py
@@ -6,6 +6,16 @@ from whispy_lispy import lexer
 
 
 class NonLispySyntaxTestCase(unittest.TestCase):
+    """Currently, there is experimental (and highly buggy) support
+    for the non-lisp-like syntax for function calls that instead of
+    using `(func arg1 arg2 ...)`, uses `func(arg1 arg2 ...)`.
+
+    Although this works for calling simple builtin functions, and even
+    for simple variable assignment, lambdas and defining functions, don't
+    work.
+
+    Probably work won't continue too much here.
+    """
     def test_non_lispy_syntax_is_corrected(self):
         # sum(1 2 sum(3 4)) -> (sum 1 2 (sum 3 4))
         original_tokens = lexer.get_flat_token_list('sum(1 2 sum(3 4))')
